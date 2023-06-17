010b188a89ecdb798f6c64a234bda45233ccfa54
Sochy27 <diarasochy@gmail.com>
2023-06-17 03:16:36 -0700

    Task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,24 +7,29 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	int i, j, k;
 
 	i = 0;
-	while (i < 9)
+	while (i < 8)
 	{
 		j = i + 1;
-		while (j <= 9)
+		while (j < 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-
-			if (!(i == 8 && j == 9))
+			k = j + 1;
+			while (k <= 9)
 			{
-				putchar(',');
-				putchar(' ');
-			}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+				k++;
+			}
 			j++;
 		}
 		i++;
