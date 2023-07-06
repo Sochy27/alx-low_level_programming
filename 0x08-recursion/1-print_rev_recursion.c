88d445f0e747ef07336ff3b30bf43463c36a67af
Sochy27 <diarasochy@gmail.com>
2023-07-06 09:17:26 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
- */
-int _strlen_recursion(char *s)
+ * _print_rev_recursion - prints out a string of characters
+ * @s: The string to be printed out
+*/
+void _print_rev_recursion(char *s)
 {
-	int longit = 0;
-	if (*s)
-	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
-	}
-	return (longit);
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
