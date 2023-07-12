008060954602b0f691715e7f76fc0545e738072a
Sochy27 <diarasochy@gmail.com>
2023-07-12 11:52:09 -0700

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,43 @@
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
+ * str_concat - function that concatenates two strings.
  *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: NULL on failure
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int len, len1, len2, i, j;
+	char *conStr;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)if (s2 == NULL)
+		s2 = "";
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	len = len1 + len2;
+	conStr = malloc(sizeof(char) * (len + 1));
+
+	if (conStr == NULL)
+	{
+		free(conStr);
 		return (NULL);
-
-	i = 0;
-	j = 0;
-
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
 	}
+	for (i = 0; i < len1; i++)
+		conStr[i] = s1[i];
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
-
-	return (s3);
+	j = len2;
+	for (len2 = 0; len2 <= j; i++, len2++)
+		conStr[i] = s2[len2];
+	return (conStr);
 }
